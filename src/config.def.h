43fd4a58af6424aa2a19b278dcfa0e5d0a07309f
sarrost <herbert@magaya.co.za>
2021-02-18 23:32:43 +0200

    Add and apply 'dmenu-alpha-20200618.diff'.

--- src/config.def.h
+++ src/config.def.h
@@ -6,13 +6,22 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"monospace:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const unsigned int bgalpha = 0xc8;
+static const unsigned int fgalpha = OPAQUE;
+static const char *prompt         = NULL;    /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
+	/*               fg         bg        */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel]  = { "#eeeeee", "#005577" },
+	[SchemeOut]  = { "#000000", "#00ffff" },
 };
+static const unsigned int alphas[SchemeLast][2] = {
+	/*	             fgalpha	bgalphga */
+	[SchemeNorm] = { fgalpha, bgalpha },
+	[SchemeSel]  = { fgalpha, bgalpha },
+	[SchemeOut]  = { fgalpha, bgalpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
