5b9174bd04768701b3482bf47343576748a3ef87
sarrost <herbert@magaya.co.za>
2021-02-19 23:27:29 +0200

    Add and apply 'dmenu-password-4.9.diff'.

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
-dmenu - dynamic menu
-====================
+# dmenu - dynamic menu
 dmenu is an efficient dynamic menu for X.
 
 
-Requirements
-------------
+## Requirements
 In order to build dmenu you need the Xlib header files.
 
 
-Installation
-------------
+## Installation
 Edit config.mk to match your local setup (dmenu is installed into
 the /usr/local namespace by default).
 
@@ -18,16 +15,15 @@ Afterwards enter the following command to build and install dmenu
 
     make clean install
 
-
-Running dmenu
--------------
+## Running dmenu
 See the man page for details.
 
-Patches
--------
+## Patches
 Alpha patch ([dmenu-alpha-20200618.diff](https://gist.github.com/oibind/d17b79025765f53e99716fb1ee780c88))
 Center patch ([dmenu-center-20200111-8cd37e1.diff](https://tools.suckless.org/dmenu/patches/center/))
 Border patch ([dmenu-borderoption-20200217-bf60a1e.diff ](https://tools.suckless.org/dmenu/patches/border/))
 Fuzzy match patch ([dmenu-fuzzymatch-4.9.diff](https://tools.suckless.org/dmenu/patches/fuzzymatch/))
-Fuzzy highlight patch ([dmenu-fuzzyhighlight-4.9.diff](https://tools.suckless.org/dmenu/patches/fuzzyhighlight/))
+Fuzzy highlight patch (disabled due to issues with transparency) ([dmenu-fuzzyhighlight-4.9.diff](https://tools.suckless.org/dmenu/patches/fuzzyhighlight/))
 Case insensitive patch ([dmenu-caseinsensitive-5.0.diff](https://tools.suckless.org/dmenu/patches/case-insensitive/))
+Line height patch ([dmenu-lineheight-5.0.diff](https://tools.suckless.org/dmenu/patches/line-height/))
+Password patch ([dmenu-password-4.9.diff](https://tools.suckless.org/dmenu/patches/password/))
--- src/dmenu.c
+++ src/dmenu.c
@@ -44,7 +44,7 @@ struct item {
 static char text[BUFSIZ] = "";
 static char *embed;
 static int bh, mw, mh;
-static int inputw = 0, promptw;
+static int inputw = 0, promptw, passwd = 0;
 static int lrpad; /* sum of left and right padding */
 static size_t cursor;
 static struct item *items = NULL;
@@ -233,6 +233,7 @@ drawmenu(void)
 	unsigned int curpos;
 	struct item *item;
 	int x = 0, y = 0, fh = drw->fonts->h, w;
+	char *censort;
 
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	drw_rect(drw, 0, 0, mw, mh, 1, 1);
@@ -244,7 +245,12 @@ drawmenu(void)
 	/* draw input field */
 	w = (lines > 0 || !matches) ? mw - x : inputw;
 	drw_setscheme(drw, scheme[SchemeNorm]);
-	drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
+	if (passwd) {
+		censort = ecalloc(1, sizeof(text));
+		memset(censort, '.', strlen(text));
+		drw_text(drw, x, 0, w, bh, lrpad / 2, censort, 0);
+		free(censort);
+	} else drw_text(drw, x, 0, w, bh, lrpad / 2, text, 0);
 
 	curpos = TEXTW(text) - TEXTW(&text[cursor]);
 	if ((curpos += lrpad / 2 - 1) < w) {
@@ -711,6 +717,11 @@ readstdin(void)
 	size_t i, imax = 0, size = 0;
 	unsigned int tmpmax = 0;
 
+	if(passwd) {
+		inputw = lines = 0;
+		return;
+	}
+
 	/* read each line from stdin and add it to the item list */
 	for (i = 0; fgets(buf, sizeof buf, stdin); i++) {
 		if (i + 1 >= size / sizeof *items)
@@ -893,7 +904,7 @@ setup(void)
 static void
 usage(void)
 {
-	fputs("usage: dmenu [-bcCfLiIv] [-bw borderwidth] [-fn font] [-fz] [-Fz] [-h height]\n"
+	fputs("usage: dmenu [-bcCfLiIPv] [-bw borderwidth] [-fn font] [-fz] [-Fz] [-h height]\n"
 				"             [-l lines] [-m monitor] [-nb color] [-nf color] [-nhb color]\n"
 				"             [-nhf color] [-p prompt] [-sb color] [-sf color] [-shb color]\n"
 				"             [-shf color] [-w windowid]\n" , stderr);
@@ -926,7 +937,9 @@ main(int argc, char *argv[])
 		} else if (!strcmp(argv[i], "-I")) { /* case-sensitive item matching */
 			fstrncmp = strncmp;
 			fstrstr = strstr;
-		} else if (!strcmp(argv[i], "-v")) { /* prints version information */
+		} else if (!strcmp(argv[i], "-P"))   /* input is a password */
+			passwd = 1;
+		else if (!strcmp(argv[i], "-v")) { /* prints version information */
 			puts("dmenu-"VERSION);
 			exit(0);
 		} else if (i + 1 == argc)
